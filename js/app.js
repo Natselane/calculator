6a06d2b2eba9b1c5aad876de2e449f6d9aaa4360
Natselane <fabien1308@gmail.com>
2020-09-07 20:44:11 +0200

    Change file names

--- js/app.js
+++ js/app.js
@@ -1,59 +1,86 @@
-let currentNumbers = [];
-let currentNum = 0; 
-let result = 0; 
+let firstNum = "";
+let secondNum = "";
 let currentSign = "";
+let result = 0;
+
 const numButtons = document.querySelectorAll(".numBtn");
 const signButtons = document.querySelectorAll(".signBtn");
-const display = document.querySelector(".added-num");
-const resultDisplay = document.querySelector(".result");
+const equalsButton = document.querySelector("#equals");
+const resultDisplay = document.querySelector("#result");
+const backspaceBtn = document.querySelector("#backspace")
 const clearBtn = document.querySelector("#clear");
-const backspaceBtn = document.querySelector("#backspace");
 
-// Evaluate expressions
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
-function operate(operator, num1, num2) {
-    if (operator === "add") {
-        return add(num1, num2);
-    }
-};
-
-function displayNumbers(numbers) {
-    let numDis = numbers.join(' ');
-    display.innerText = numDis;
-}
-
-function showResult(result) {
-    resultDisplay.innerText = result;
-}
-
 function reset() {
-    showResult(0);
-    currentNumbers = [];
-    currentNum = 0;
+    firstNum = "";
+    secondNum = "";
+    currentSign = "";
     result = 0;
-    displayNumbers(["0"]);
+    resultDisplay.textContent = "0";
+}
+
+function toDisplay(displayValue) {
+    resultDisplay.textContent = displayValue;
+}
+
+function getNumber(numSum, num) {
+    numSum += num; 
+    toDisplay(numSum);
+    return numSum;
+}
+
+function doOperation() {
+    if (currentSign === "+") {
+        result = add(parseInt(firstNum), parseInt(secondNum));
+     } else if (currentSign === "-") {
+         result = subtract(parseInt(firstNum), parseInt(secondNum));
+     } else if (currentSign === "*") {
+         result = multiply(parseInt(firstNum), parseInt(secondNum));
+     } else if (currentSign === "/") {
+         result = divide(parseInt(firstNum), parseInt(secondNum));
+     } 
+    toDisplay(result);
 }
 
 numButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        currentNum = button.innerText;
-        console.log(currentNum);
-        currentNumbers.push(currentNum);
-        displayNumbers(currentNumbers);
-        showResult(result);
-    });
+    button.addEventListener('click', () => {
+        let number = button.textContent;
+        if (currentSign === "") {
+            firstNum = getNumber(firstNum, number);
+        } else {
+            secondNum = getNumber(secondNum, number);
+        }
+    })
 })
 
 signButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        currentSign = button.innerText;
-        currentNumbers.push(currentSign);
-        displayNumbers(currentNumbers);
-    });
+    button.addEventListener('click', () => {
+        currentSign = button.textContent;
+    })
 })
 
-clearBtn.addEventListener("click", () => reset());
+equalsButton.addEventListener("click", () => {
+    doOperation();
+    firstNum = result;
+    secondNum = "";
+    currentSign = "";
+})
+
+backspaceBtn.addEventListener("click", () => {
+    console.log("backspace");
+    if (currentSign === "") {
+        firstNum = firstNum.slice(0, firstNum.length - 1);
+        toDisplay(firstNum);
+    } else {
+        secondNum = secondNum.slice(0, secondNum.length - 1);
+        toDisplay(secondNum);
+    }
+})
+
+clearBtn.addEventListener("click", () => {
+    reset();
+})
